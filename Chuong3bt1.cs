0e1682a6c4ac05dd27e4e7698e2497a03d62b4bc
muaxualo123 <hung01645167156@gmail.com>
2021-11-20 17:37:28 +0700

    Create Chuong3bt1.cs

---
+++ Chuong3bt1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chuong3_bt1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 0;
+            int c = 0;
+            Console.WriteLine("nhap a: ",a);
+            a =int.Parse(Console.ReadLine());
+            Console.WriteLine("nhap b: ", b);
+            b =int.Parse(Console.ReadLine());
+            Console.WriteLine($"+ :c={c = a + b}");
+            Console.WriteLine($"- :c={c = a - b}");
+            Console.WriteLine($"* :c={c = a * b}");
+            Console.WriteLine($"/: c={c = a / b}");
+            Console.ReadKey();
+
+
+        }
+    }
+}
