f9c78c29f0d5716cf4aaa1ec5b298ecb952ad9b7
đỗ phan duy hung <91817670+muaxualo123@users.noreply.github.com>
2021-11-20 17:39:14 +0700

    Add files via upload

---
+++ Baitap3_ch3.cs
@@ -0,0 +1,34 @@
+﻿/*3.	Viết chương trình nhập vào điểm ba môn Toán, Lý, Hóa của một học sinh. In ra điểm trung bình của học sinh đó với hai số lẻ thập phân.
+do phan duy hùng
+cd21tt11
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bt3ch3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Toan = 0;
+            double Ly = 0;
+            double Hoa = 0;
+            double diemtrungbinh = 0;
+            Console.WriteLine("toan", Toan);
+            Toan = double.Parse(Console.ReadLine());
+           
+            
+            Console.WriteLine("ly",Ly);
+            Ly = double.Parse(Console.ReadLine());
+            Console.WriteLine("hoa",Hoa);
+            Hoa = double.Parse(Console.ReadLine());
+            
+            Console.WriteLine($"diemtrungbinh = {(Toan + Ly + Hoa) / 3}",diemtrungbinh);
+            Console.ReadKey();
+        }
+    }
+}
